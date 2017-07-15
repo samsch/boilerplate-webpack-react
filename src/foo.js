85ad5dbf8a525b61a5846b81dedabe5aa3bdcdf9
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2017-07-14 21:08:19 -0400

    Adds css-loader and extract text plugin for loading styles.

--- src/foo.js
+++ src/foo.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: ['babel-polyfill', './src/main.js'],
@@ -17,6 +18,18 @@ module.exports = {
         include: path.resolve(__dirname, 'src/'),
         use: ['babel-loader'],
       },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
     ],
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true,
+    }),
+  ],
 };
