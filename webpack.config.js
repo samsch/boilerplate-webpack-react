17619b421584c3ef1fe2a999a1b525ca7d38bbd5
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2017-12-27 09:45:55 -0500

    Fixes NODE_ENV define not working (bad production builds).

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 /* eslint-env node */
+const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -68,6 +69,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new ExtractTextPlugin({
       filename: 'style.css',
       allChunks: true,
