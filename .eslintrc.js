4ff26584742d98333280b7ae6ec6c71df0175e91
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2018-02-16 15:35:48 -0500

    Removed prettier and added new eslint config focused on catching errors, not forcing styles.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
   },
   parser: 'babel-eslint',
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
@@ -15,12 +15,34 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     semi: ['error', 'always'],
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-      },
-    ],
+    'array-callback-return': 'warn',
+    eqeqeq: ["error", "always", {"null": "ignore"}],
+    'no-empty-function': 'warn',
+    'no-eval': 'warn',
+    'no-implied-eval': 'warn',
+    'no-invalid-this': 'error',
+    'no-param-reassign': 'error',
+    'no-return-assign': 'error',
+    'no-return-await': 'error',
+    'no-self-compare': 'warn',
+    'no-useless-concat': 'warn',
+    'no-useless-return': 'warn',
+    'no-void': 'error',
+    'no-warning-comments': 'warn',
+    'no-with': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'wrap-iife': ['error', 'inside'],
+    'no-shadow-restricted-names': 'error',
+    'no-use-before-define': 'error',
+    'quotes': ["warn", "single"],
+    'space-before-function-paren': 'error',
+    'no-trailing-spaces': 'error',
+    'arrow-spacing': 'error',
+    'no-duplicate-imports': 'error',
+    'no-useless-rename': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+
+    "react/prop-types": ['error', { skipUndeclared: true }]
   },
 };
