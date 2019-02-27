cd1f69e59bed797b54a24820a0da081bc63a6ee3
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2019-02-27 09:08:47 -0500

    Replaced App class with App function component using Hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React from 'react';
 import styles from './style.css';
 
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-    this.inc = this.inc.bind(this);
-    this.dec = this.dec.bind(this);
+const App = () => {
+  const [count, setCount] = React.useState(0);
+  function inc () {
+    setCount(count => count + 1);
   }
-  inc () {
-    this.setState(state => ({ count: state.count + 1 }));
+  function dec () {
+    setCount(count => count - 1);
   }
-  dec () {
-    this.setState(state => ({ count: state.count - 1 }));
-  }
-  render () {
-    const { count } = this.state;
-    return (
-      <div>
-        <h1 className={styles.heading}>Counter Example</h1>
-        <p className={styles.output} >Count: {count}</p>
-        <p>
-          <button type="button" onClick={this.inc} >Inc</button>
-          {' '}
-          <button type="button" onClick={this.dec} >Dec</button>
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1 className={styles.heading}>Counter Example</h1>
+      <p className={styles.output} >Count: {count}</p>
+      <p>
+        <button type="button" onClick={inc} >Inc</button>
+        {' '}
+        <button type="button" onClick={dec} >Dec</button>
+      </p>
+    </div>
+  );
+};
 
 export default App;
